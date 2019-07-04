dd4241df858b0b54cdbdba75f24c622412cccbf3
alessanderleite <alessander.logistica@gmail.com>
2019-07-03 21:55:30 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/SpringbootJwtSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtSampleApplication.class, args);
+	}
+
+}
