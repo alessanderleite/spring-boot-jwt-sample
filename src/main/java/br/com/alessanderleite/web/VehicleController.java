e553fa51fe7df2b38f3164478f19dc72883eeebc
alessanderleite <alessander.logistica@gmail.com>
2019-07-05 22:27:44 -0300

    code the VehicleController class

--- src/main/java/br/com/alessanderleite/web/VehicleController.java
+++ src/main/java/br/com/alessanderleite/web/VehicleController.java
@@ -1,14 +1,21 @@
 package br.com.alessanderleite.web;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.alessanderleite.domain.Vehicle;
 import br.com.alessanderleite.repository.VehicleRepository;
 
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.noContent;
 import static org.springframework.http.ResponseEntity.ok;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +37,35 @@ public class VehicleController {
 	
 	@PostMapping("")
 	public ResponseEntity save(@RequestBody VehicleForm form, HttpServletRequest request) {
+		Vehicle saved = this.vehicles.save(Vehicle.builder().name(form.getName()).build());
+		return created(
+			ServletUriComponentsBuilder
+				.fromContextPath(request)
+				.path("/v1/vehicles/{id}")
+				.buildAndExpand(saved.getId())
+				.toUri())
+			.build();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity get(@PathVariable("id") Long id) {
+		return ok(this.vehicles.findById(id).orElseThrow(()-> new VehicleNotFoundException()));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity update(@PathVariable("id") Long id, @RequestBody VehicleForm form) {
+		Vehicle existed = this.vehicles.findById(id).orElseThrow(()-> new VehicleNotFoundException());
+		existed.setName(form.getName());
 		
+		this.vehicles.save(existed);
+		return noContent().build();
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping("/{id}")
+	public ResponseEntity delete(@PathVariable("id") Long id) {
+		Vehicle existed = this.vehicles.findById(id).orElseThrow(()-> new VehicleNotFoundException());
+		this.vehicles.delete(existed);
+		return noContent().build();
 	}
 }
