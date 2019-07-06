503bfd2309139df25bc726806dc926feb2d66076
alessanderleite <alessander.logistica@gmail.com>
2019-07-06 20:57:38 -0300

    the abstract class for persistance

---
+++ src/main/java/br/com/alessanderleite/domain/AbstractPersistableEntity.java
@@ -0,0 +1,30 @@
+package br.com.alessanderleite.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class AbstractPersistableEntity<ID> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private ID id;
+	
+	@Version
+	private Long version;
+}
