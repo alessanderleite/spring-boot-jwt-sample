4529d8db0edf521301783db9d2ac797f38d8d1a9
alessanderleite <alessander.logistica@gmail.com>
2019-07-06 20:52:14 -0300

    add annotation

--- src/main/java/br/com/alessanderleite/repository/VehicleRepository.java
+++ src/main/java/br/com/alessanderleite/repository/VehicleRepository.java
@@ -1,8 +1,10 @@
 package br.com.alessanderleite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.alessanderleite.domain.Vehicle;
 
+@RepositoryRestResource(path = "vehicles", collectionResourceRel = "vehicles", itemResourceRel = "vehicle")
 public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
 }
